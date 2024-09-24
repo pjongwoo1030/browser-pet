9d7aa4660c9a54f4021f4a77a089946434ee08a4
Jongwoo Park <jongwoo@my.yorku.ca>
2024-09-24 17:24:53 -0400

    Snorlax can now be parked! He will sit nice and quietly.

--- pixel-pet.js
+++ pixel-pet.js
@@ -12,39 +12,60 @@ document.body.appendChild(petIcon);
 // Snorlax interaction logic
 const hoverImage = chrome.runtime.getURL('pets/snorlax_sleep.png');
 const defaultImage = chrome.runtime.getURL('pets/snorlax_rock.png');
+const parkedImage = chrome.runtime.getURL('pets/snorlax_sit.png');
 let isBouncing = false;
+let isParked = false;
+let clickTimer = null;
 
-snorlax.addEventListener('click', function() {
-  if (isBouncing) return;
+// Double-click event listener
+snorlax.addEventListener('dblclick', function () {
+  clearTimeout(clickTimer);
 
-  let posY = 0;
-  let direction = 1;
-  const speedY = 0.7;
-  const maxY = petIcon.offsetHeight / 4;
-  snorlax.style.animationPlayState = 'paused';
-
-  let bounceInterval = setInterval(function() {
-    isBouncing = true;
-    snorlax.src = hoverImage;
-    posY += speedY * direction;
-    if (posY >= maxY) {
-      direction *= -1;
-    } else if (posY <= 0) {
-      clearInterval(bounceInterval);
-      direction = 1;
-      snorlax.src = defaultImage;
-      isBouncing = false;
-      snorlax.style.animationPlayState = 'running';
-    }
-    petIcon.style.bottom = posY + 'px';
-  }, 16);
+  if (!isParked) {
+    snorlax.style.animationPlayState = 'paused';
+    snorlax.src = parkedImage;
+  } else {
+    snorlax.style.animationPlayState = 'running';
+    snorlax.src = defaultImage;
+  }
+  isParked = !isParked;
 });
+
+// Single-click event listener
+snorlax.addEventListener('click', function () {
+  clickTimer = setTimeout(() => {
+    if (isBouncing || isParked) return;
+
+    let posY = 0;
+    let direction = 1;
+    const speedY = 0.7;
+    const maxY = petIcon.offsetHeight / 4;
+    snorlax.style.animationPlayState = 'paused';
+
+    let bounceInterval = setInterval(function () {
+      isBouncing = true;
+      snorlax.src = hoverImage;
+      posY += speedY * direction;
+      if (posY >= maxY) {
+        direction *= -1;
+      } else if (posY <= 0) {
+        clearInterval(bounceInterval);
+        direction = 1;
+        snorlax.src = defaultImage;
+        isBouncing = false;
+        snorlax.style.animationPlayState = 'running';
+      }
+      petIcon.style.bottom = posY + 'px';
+    }, 16);
+  }, 200);
+});
+
 
 let posX = 0;
 const speedX = 0.2;
 let directionX = 1;
 function movePet() {
-  if (!isBouncing) {
+  if (!isBouncing && !isParked) {
     posX += speedX * directionX;
     if (posX > window.innerWidth - petIcon.offsetWidth || posX <= 0) {
       directionX *= -1;
